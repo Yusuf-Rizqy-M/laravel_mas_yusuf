d2f175c0a402284698f5e4f2d5d94f055d98cf7c
Yusuf Rizqy M <yusufhabib290@gmail.com>
2025-01-16 08:17:15 +0700

    Feat:[YR] exercise laravel

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+{{-- <x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>Halaman Home</h3>
+</x-layout> --}}
+
+<x-admin-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <x-main></x-main>
+</x-admin-layout>
